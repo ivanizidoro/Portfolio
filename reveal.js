c3b96d6d8aebfa26552ba87e14f326abe4d8f67b
ivanizidoro <ivanizidoro@hotmail.com>
2023-11-05 21:13:27 -0300

    adicionado efeito scroll, javascript

---
+++ reveal.js
@@ -0,0 +1,132 @@
+window.revelar = ScrollReveal({reset:true})
+
+
+// TOPO DO SITE
+
+revelar.reveal('.efeito-txt-topo', {
+    duration: 2000,
+    distance: '90px'
+})
+
+revelar.reveal('.efeito-img-topo', {
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+})
+
+//OS TITULOS DAS SESSÔES
+
+revelar.reveal('.titulo', {
+    duration: 2000,
+    distance: '90px'
+})
+
+// SESSÃO DE ESPECIALIDADES
+
+
+revelar.reveal('.efeito-especialidades1', {
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+})
+
+revelar.reveal('.efeito-especialidades2', {
+    duration: 2000,
+    distance: '90px',
+    delay: 1000
+})
+
+revelar.reveal('.efeito-especialidades3', {
+    duration: 2000,
+    distance: '90px',
+    delay: 1500
+})
+
+revelar.reveal('.efeito-especialidades4', {
+    duration: 2000,
+    distance: '90px',
+    delay: 2000
+})
+
+revelar.reveal('.efeito-especialidades5', {
+    duration: 2000,
+    distance: '90px',
+    delay: 2500
+})
+
+// SESSÃO SOBRE
+
+revelar.reveal('.efeito-img-sobre', {
+    duration: 2000,
+    distance: '90px',
+    delay: 500,
+    origin: 'left'
+})
+
+revelar.reveal('.efeito-txt-sobre', {
+    duration: 2000,
+    distance: '90px',
+    delay: 1000,
+    origin: 'rigth'
+})
+
+// SESSÃO PORTFOLIO
+
+revelar.reveal('.efeito-txt-port1', {
+    duration: 2000,
+    distance: '90px',
+    delay: 500,
+    origin: 'bottom'
+})
+
+revelar.reveal('.efeito-txt-port2', {
+    duration: 2000,
+    distance: '90px',
+    delay: 1000,
+    origin: 'bottom'
+})
+
+revelar.reveal('.efeito-txt-port3', {
+    duration: 2000,
+    distance: '90px',
+    delay: 1500,
+    origin: 'bottom'
+})
+
+// SESSÃO CONTATO
+
+
+revelar.reveal('.efeito-ipunt1', {
+    duration: 2000,
+    distance: '90px',
+    delay: 500,
+    origin: 'bottom'
+})
+
+revelar.reveal('.efeito-ipunt2', {
+    duration: 2000,
+    distance: '90px',
+    delay: 700,
+    origin: 'bottom'
+})
+
+revelar.reveal('.efeito-ipunt3', {
+    duration: 2000,
+    distance: '90px',
+    delay: 900,
+    origin: 'bottom'
+})
+
+revelar.reveal('.efeito-mensangem', {
+    duration: 2000,
+    distance: '90px',
+    delay: 1100,
+    origin: 'bottom'
+})
+
+revelar.reveal('.efeito-enviar', {
+    duration: 2000,
+    distance: '90px',
+    delay: 1300,
+    origin: 'bottom'
+})
